108484f83ec12be1e4fb0a40fc5c78f0015e91a6
Weng Xuetian <wengxt@gmail.com>
2013-08-05 18:16:28 -0400

    [kkc] add license

--- gui/common.h
+++ gui/common.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2012~2012 by CSSlayer                                   *
+ *   Copyright (C) 2013~2013 by CSSlayer                                   *
  *   wengxt@gmail.com                                                      *
  *                                                                         *
  *  This program is free software: you can redistribute it and/or modify   *
